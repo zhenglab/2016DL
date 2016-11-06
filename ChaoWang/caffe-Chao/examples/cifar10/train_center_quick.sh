286b5d0112abd8570fc165328561cb847af1f806
godisboy <xgsbsz@gmail.com>
2016-11-06 19:49:09 +0800

    add center loss for caffe

---
+++ ChaoWang/caffe-Chao/examples/cifar10/train_center_quick.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -e
+
+TOOLS=./build/tools
+
+$TOOLS/caffe train \
+  --solver=examples/cifar10/cifar10_center_quick_solver.prototxt $@
+
--- ChaoWang/caffe-Chao/examples/cifar10/train_full.sh
+++ ChaoWang/caffe-Chao/examples/cifar10/train_full.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
-export PYTHONPATH=$PWD/python:$PWD/examples/cifar10
+set -e
+
 TOOLS=./build/tools
 
 $TOOLS/caffe train \
-    --solver=examples/cifar10/cifar10_full_solver.prototxt
+    --solver=examples/cifar10/cifar10_full_solver.prototxt $@
 
+# reduce learning rate by factor of 10
+$TOOLS/caffe train \
+    --solver=examples/cifar10/cifar10_full_solver_lr1.prototxt \
+    --snapshot=examples/cifar10/cifar10_full_iter_60000.solverstate.h5 $@
+
+# reduce learning rate by factor of 10
+$TOOLS/caffe train \
+    --solver=examples/cifar10/cifar10_full_solver_lr2.prototxt \
+    --snapshot=examples/cifar10/cifar10_full_iter_65000.solverstate.h5 $@
